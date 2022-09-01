0b50680e7cb137558a83916b85187de8324e8529
gobinmateo <mateo.gobin@gmail.com>
2022-09-01 06:48:34 -0700

    fix app router import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { withTRPC } from '@trpc/next';
 import { AppType } from 'next/dist/shared/lib/utils';
 import superjson from 'superjson';
-import { AppRouter } from './api/trpc/[trpc]';
+import { AppRouter } from '../backend/router';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;

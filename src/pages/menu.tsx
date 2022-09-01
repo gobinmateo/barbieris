b5b06b5632b41e99aca979c775b52ddd564a23bf
gobinmateo <mateo.gobin@gmail.com>
2022-09-01 05:43:06 -0700

    setup tRPC

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,4 +1,11 @@
+import { trpc } from '../utils/trpc';
+
 function Menu(props: any) {
+  const { data, isLoading } = trpc.useQuery(['hello', { text: 'abcd' }]);
+
+  if (isLoading || !data) return <div>Loading...</div>;
+
+  console.log('data', data);
   return (
     <>
       <h1 className="text-3xl">Menu</h1>
